b61389f15484b2922eca3e0865820474fc117c27
Akash <awesomeakash47@gmail.com>
2026-05-19 15:31:57 +0530

    added percentage tax support

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
-def calculate_total(price, quantity):
+def calculate_total(price, quantity, tax_percent=5):
     total = price * quantity
+    total += total * tax_percent / 100
     return total
-
 
 def main():
     price = 100
     quantity = 2
 
-    total = calculate_total(price, quantity)
+    total = calculate_total(price, quantity, tax_percent=10)
+    
     print(f"Total amount: {total}")
 
 
